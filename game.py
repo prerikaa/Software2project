c05ad3bdd67d924e4958572d7d4ca2663a2b5eb1
monika11318 <monikatiwari2419@gmail.com>
2026-05-08 10:06:52 +0300

    Refuel function

--- game.py
+++ game.py
@@ -132,5 +132,6 @@ def refuel():
         print(f"Error in /refuel: {e}")
         return jsonify({"success": False, "message": "Refueling system offline."}), 500
 
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
